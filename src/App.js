1359a8a4bf5de5ed8cd4b7100d1c2e05c65260b6
Almog E <almog.eziony@live.biu.ac.il>
2025-02-08 16:55:43 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Almog </h1>
+
+        <h2>Experience</h2>
+
+
+        <h2>Education</h2>
+        <p>Computer Science, Year 3, Bar-Ilan University</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
